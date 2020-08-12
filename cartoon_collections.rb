6d78bc21ff10e7d9182cc2fcd009f03554b2de9f
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-12 18:55:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index { |dwarf, index|
-  puts "#{(index+=1)}. #{dwarf}"
+  puts "#{(index+1)}. #{dwarf}"
   }
 end
